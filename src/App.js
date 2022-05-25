9ced202ec39656d4aa09d90f4254fd56e8c28114
mw <maweltscher@gmail.com>
2022-05-25 12:19:31 +0300

    Moved provider tag to index.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Support from "./components/Support";
 import Customer from "./components/Customer";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+
 import Login from "./components/Login";
 
 const App = () => {
 
 
     return (
-        <>
-            <Provider store={store}>
                 <Router>
                     <Switch>
                         <Route exact path='/Chat'><Customer /></Route>
@@ -19,8 +16,6 @@ const App = () => {
                         <Route exact path='/Admin'><Support /></Route>
                     </Switch>
                 </Router>
-            </Provider>
-        </>
     );
 
 };
